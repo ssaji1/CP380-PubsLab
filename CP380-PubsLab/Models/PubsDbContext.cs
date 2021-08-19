2d5e2a93ddb98e87194dc17842450b2028524219
Saneesh Saji <65639829+ssaji1@users.noreply.github.com>
2021-08-19 19:46:54 -0400

    completed lab

--- CP380-PubsLab/Models/PubsDbContext.cs
+++ CP380-PubsLab/Models/PubsDbContext.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using System.IO;
-
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq.Expressions;
 
 namespace CP380_PubsLab.Models
 {
@@ -12,12 +15,29 @@ namespace CP380_PubsLab.Models
             var dbpath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\pubs.mdf"));
             optionsBuilder.UseSqlServer($"Server=(localdb)\\mssqllocaldb;Integrated Security=true;AttachDbFilename={dbpath}");
         }
-
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<Jobs> Jobs { get; set; }
+        public DbSet<Titles> Titles { get; set; }
+        public DbSet<Stores> Stores { get; set; }
+        public DbSet<Sales> Sales { get; set; }
         // TODO: Add DbSets
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // TODO
+            modelBuilder.Entity<Sales>()
+               .HasOne(NewMethod())
+               .WithMany(b => b.Sales)
+               .HasForeignKey(bc => bc.stor_id);
+            modelBuilder.Entity<Sales>()
+                .HasOne(bc => bc.Title)
+                .WithMany(c => c.Sales)
+                .HasForeignKey(bc => bc.title_id);
+        }
+
+        private static Expression<Func<Sales, Stores>> NewMethod()
+        {
+            return bc => bc.Store;
         }
     }
 
@@ -25,27 +45,72 @@ namespace CP380_PubsLab.Models
     public class Titles
     {
         // TODO
+        [Key]
+        public string title_id { get; set; }
+        public string title { get; set; }
+        public string type { get; set; }
+        public Decimal? price { get; set; }
+        public Decimal? advance { get; set; }
+        public int? royalty { get; set; }
+        public int? ytd_sales { get; set; }
+        public string? notes { get; set; }
+        public DateTime pubdate { get; set; }
+
+        public List<Sales> Sales { get; set; }
     }
 
 
-    public class Stores
+    public class Stores 
     {
         // TODO
+        [Key]
+        public string stor_id { get; set; }
+        public string stor_name { get; set; }
+        public string stor_address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+
+        public List<Sales> Sales { get; set; }
     }
 
 
     public class Sales
     {
         // TODO
+        [Key]
+        public string ord_num { get; set; }
+        public DateTime ord_date { get; set; }
+        public Int16 qty { get; set; }
+        public string payterms { get; set; }
+
+        public string stor_id { get; set; }
+        [ForeignKey("stor_id")]
+        public Stores Store { get; set; }
+
+        public string title_id { get; set; }
+        [ForeignKey("title_id")]
+        public Titles Title { get; set; }
     }
 
     public class Employee
     {
         // TODO
+        [Key]
+        public string emp_id { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public Int16 job_id { get; set; }
+        [ForeignKey("job_id")]
+        public Jobs Job { get; set; }
     }
 
     public class Jobs
     {
         // TODO
+        [Key]
+        public Int16 job_id { get; set; } 
+        public string job_desc { get; set; }
+        public List<Employee> Employees { get; set; }
     }
 }
--- CP380-PubsLab/Program.cs
+++ CP380-PubsLab/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
 
 namespace CP380_PubsLab
 {
@@ -17,7 +20,34 @@ namespace CP380_PubsLab
                 //
                 // TODO: - Loop through each employee
                 //       - For each employee, list their job description (job_desc, in the jobs table)
+                var employee = dbcontext.Employee
+                    .Select(emp => new {
+                        emp_id = emp.emp_id,
+                        fname = emp.fname,
+                        lname = emp.lname,
+                        job_desc = emp.Job.job_desc
+                    })
+                    .ToList();                         
+                Console.WriteLine("EMPLOYEE JOB DESCRIPTION\n\n");
+                Console.WriteLine("Name  \t\t\t Job Description\n\n");
+                foreach (var emp in employee)          
+                {
+                    Console.WriteLine(emp.fname + " " + emp.lname + "\t\t\t" + emp.job_desc); 
+                }
 
+                var job_list = dbcontext.Jobs.ToList();
+                Console.WriteLine("\n\nLIST OF EMPLOYEES BASED ON JOB\n\n");
+                foreach (var job in job_list)         
+                {
+                    Console.WriteLine("\nList of Employees in " + job.job_desc);
+                    var job_emp = dbcontext.Employee
+                                  .Where(emp => emp.job_id == job.job_id)
+                                  .ToList();
+                    foreach (var emp in job_emp)   
+                    {
+                        Console.WriteLine(emp.fname + " " + emp.lname);
+                    }
+                }
                 // TODO: - Loop through all of the jobs
                 //       - For each job, list the employees (first name, last name) that have that job
 
@@ -29,7 +59,45 @@ namespace CP380_PubsLab
                 //
                 // e.g.
                 //  Bookbeat -> The Gourmet Microwave, The Busy Executive's Database Guide, Cooking with Computers: Surreptitious Balance Sheets, But Is It User Friendly?
-                
+                var stores = dbcontext.Stores.ToList();
+                var titles = dbcontext.Titles.ToList();
+                var sales = dbcontext.Sales.ToList();
+
+                Console.WriteLine("\n\n\n Details of Books Sold at Every Store\n");
+                foreach (var store in stores)
+                {
+                    Console.Write(store.stor_name + " -> ");
+                    var sale_list = sales.Where(str => str.stor_id == store.stor_id).ToList();
+                    var i = 0;
+                    foreach (var sale in sale_list)
+                    {
+                        if (i != 0)
+                        {
+                            Console.Write(", ");
+                        }
+                        Console.Write(titles.First(t => t.title_id == sale.title_id).title);
+                        i++;
+                    }
+                    Console.WriteLine("\n");
+                }
+
+                Console.WriteLine("\n\n\n Details of  Available Stores With This Book Details\n\n");
+                foreach (var title in titles)
+                {
+                    Console.Write(title.title + " -> ");
+                    var sale_list = sales.Where(str => str.title_id == title.title_id).ToList();
+                    var i = 0;
+                    foreach (var sale in sale_list)
+                    {
+                        if (i != 0)
+                        {
+                            Console.Write(", ");
+                        }
+                        Console.Write(stores.First(t => t.stor_id == sale.stor_id).stor_name);
+                        i++;
+                    }
+                    Console.WriteLine("\n");
+                }
                 // TODO: - Loop through each Title
                 //       - For each title, list all the stores it was sold at
                 //
